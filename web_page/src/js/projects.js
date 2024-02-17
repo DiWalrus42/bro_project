148c4d7006fe3c69d72539d8af244064653c4fb8
Hansley S <hansleysti@gmail.com>
2024-02-17 13:45:32 +0400

    added html and js file to manage projects

---
+++ web_page/src/js/projects.js
@@ -0,0 +1,14 @@
+// id of the project will be used for the link and thumbnail
+// date format to be used dd-mm-yyyy
+
+const _prj_data = {
+    items: [
+        {
+            'id': '',
+            'title': '',
+            'description' : '',
+            'author': '',
+            'date-added': '',
+        }
+    ]
+};
